86108c02c0b0e3d363ef8c917ef0c19d29d318d6
Rohan-3599 <rohansinha3599@gmail.com>
2021-02-07 09:24:31 +0530

    mern_app

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import useStyles from './styles';
+import {useSelector} from 'react-redux';
+
 const Post = () => {
+    const posts = useSelector((state) => state.posts);
     const classes = useStyles();
+    console.log(posts);
     return(
         <h1>POST</h1>
     );

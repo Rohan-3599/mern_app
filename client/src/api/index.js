7f90a2d5bfdd00a574f768f66cfb61e248a89985
Rohan-3599 <rohansinha3599@gmail.com>
2021-02-26 10:21:33 +0530

    final

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 const API = axios.create({ baseURL: 'https://mern-app-exp.herokuapp.com/' });
 API.interceptors.request.use((req) => {
     if (localStorage.getItem('profile')) {
-      req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem('profile')).token}`;
+      req.headers.authorization = `Bearer ${JSON.parse(localStorage.getItem('profile')).token}`;
     }
     return req;
 });
